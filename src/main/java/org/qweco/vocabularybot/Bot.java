c6cf9ac650904539bb8cff60e21490b6445235e5
enger <pvl.zolotov@gmail.com>
2018-07-07 22:22:42 +0300

    Firebase lib add, "add to dictionary" button created

--- src/main/java/org/qweco/vocabularybot/Bot.java
+++ src/main/java/org/qweco/vocabularybot/Bot.java
@@ -1,16 +1,35 @@
 package org.qweco.vocabularybot;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiValidationException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Bot extends TelegramLongPollingBot {
+    final private static String PHRASE_ADD_COMMAND = "➕";
+
     public static void main (String[] args){
         ApiContextInitializer.init();
         TelegramBotsApi api = new TelegramBotsApi();
@@ -45,6 +64,24 @@ public class Bot extends TelegramLongPollingBot {
     private void handleIncomingMessage(Message msg) {
         SendMessage s = new SendMessage();
         s.setChatId(msg.getChatId());
+        if (msg.getText().equals(PHRASE_ADD_COMMAND)){
+            s.setText("added");
+        }else {
+            //quick action keyboard actions
+            ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+            replyKeyboardMarkup.setSelective(true);
+            replyKeyboardMarkup.setResizeKeyboard(true);
+            replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+            List<KeyboardRow> keyboard = new ArrayList<>();
+            KeyboardRow row = new KeyboardRow();
+            row.add(PHRASE_ADD_COMMAND); //add to dictionary, notification emoji
+            //row.add("\uD83D\uDCDD"); //edit the translation, edit emoji
+            keyboard.add(row);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            s.setReplyMarkup(replyKeyboardMarkup);
+        }
+
         try {
             s.setText(Translator.translate("ru", msg.getText()));
             try {
@@ -55,5 +92,47 @@ public class Bot extends TelegramLongPollingBot {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    private void savePhrase (String phrase, int userId){
+        try {
+            // Fetch the service account key JSON file contents
+            //FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountCredentials.json");
+
+            // Initialize the app with a custom auth variable, limiting the server's access
+            Map<String, Object> auth = new HashMap<>();
+            auth.put("uid", userId);
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.getApplicationDefault())
+                    .setDatabaseUrl("https://vocabulary-bot.firebaseio.com/")
+                    .setDatabaseAuthVariableOverride(auth)
+                    .build();
+            FirebaseApp.initializeApp(options);
+
+            // The app only has access as defined in the Security Rules
+            DatabaseReference ref = FirebaseDatabase
+                    .getInstance()
+                    .getReference("/users/"+userId+"/en-ru/");
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    /*String res = dataSnapshot.getValue();
+                    System.out.println(res);*/
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    error.toException().printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setPeriodicReminder (){
+
     }
 }
