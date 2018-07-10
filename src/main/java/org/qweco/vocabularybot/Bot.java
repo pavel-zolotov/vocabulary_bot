9c5ca1508fe8098218f926c3958b7c544f44836c
enger <pvl.zolotov@gmail.com>
2018-07-10 20:42:09 +0300

    Firebase DB permission fix

--- src/main/java/org/qweco/vocabularybot/Bot.java
+++ src/main/java/org/qweco/vocabularybot/Bot.java
@@ -332,7 +332,7 @@ public class Bot extends TelegramLongPollingBot {
 
             // Initialize the app with a custom auth variable, limiting the server's access
             Map<String, Object> auth = new HashMap<>();
-            auth.put("uid", userId);
+            auth.put("uid", String.valueOf(userId));
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -391,7 +391,7 @@ public class Bot extends TelegramLongPollingBot {
 
             // Initialize the app with a custom auth variable, limiting the server's access
             Map<String, Object> auth = new HashMap<>();
-            auth.put("uid", userId);
+            auth.put("uid", String.valueOf(userId));
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
